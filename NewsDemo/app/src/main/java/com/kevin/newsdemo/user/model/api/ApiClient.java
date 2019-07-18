d76af0e3f8715b03a58597e5402d463bbca84e1e
xinru leng <xinruleng@gmail.com>
2019-07-18 16:18:18 -0700

    retrofit set time out.

--- NewsDemo/app/src/main/java/com/kevin/newsdemo/user/model/api/ApiClient.java
+++ NewsDemo/app/src/main/java/com/kevin/newsdemo/user/model/api/ApiClient.java
@@ -1,8 +1,11 @@
 package com.kevin.newsdemo.user.model.api;
 
 import com.kevin.newsdemo.user.model.UserModel;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by kevin on 2019/07/18 13:38.
@@ -16,8 +19,13 @@ public class ApiClient {
     }
 
     private ApiClient() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .build();
         final Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(UserModel.HOST)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create());
 
         retrofit = builder.build();
